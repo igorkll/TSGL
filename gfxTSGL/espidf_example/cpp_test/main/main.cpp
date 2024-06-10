f6dc39f07ba9dd6be2365aea360bb31815932020
igorkll <frerther@gmail.com>
2024-06-10 22:27:26 +0300

    Update main.cpp

--- gfxTSGL/espidf_example/cpp_test/main/main.cpp
+++ gfxTSGL/espidf_example/cpp_test/main/main.cpp
@@ -2,6 +2,8 @@
 #pragma GCC diagnostic ignored "-Wnarrowing"
 #pragma GCC diagnostic push
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <TSGL.hpp>
 #include <TSGL_drivers/st7789.h>
 
@@ -17,4 +19,11 @@ TSGL_Display display(&st7789_rgb565, driverSettings, false, TSGL_HOST1, 20000000
 extern "C" void app_main() {
     display.clear(TSGL_GREEN);
     display.update();
+
+    tsgl_display_clear(&display.display, tsgl_color_raw(TSGL_RED, display.display.colormode));
+
+    while (true) {
+        printf("loop\n")
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
