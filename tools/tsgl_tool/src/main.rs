8b1f722aced4d1066b4feaf81a8ad027b24c2fc8
igorkll <frerther@gmail.com>
2024-06-25 20:49:23 +0300

    parsing added

--- tools/tsgl_tool/src/main.rs
+++ tools/tsgl_tool/src/main.rs
@@ -1,3 +1,19 @@
+use fontdue;
+use image;
+
 fn main() {
-    println!("Hello, world!");
+    let font = include_bytes!("font.ttf") as &[u8];
+    let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
+    let (metrics, bitmap) = font.rasterize('b', 50.0);
+
+    for py in 0..metrics.height {
+        for px in 0..metrics.width {
+            if bitmap[px + (py * metrics.width)] > 100 {
+                print!("██");
+            } else {
+                print!("  ");
+            }
+        }
+        println!("");
+    }
 }
