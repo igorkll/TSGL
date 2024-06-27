2841d56f73d7ffc99afd4f97d9cf22dab2e7bf6b
igorkll <frerther@gmail.com>
2024-06-27 16:09:45 +0300

    font drawer template created

--- tools/tsgl_tool/src/main.rs
+++ tools/tsgl_tool/src/main.rs
@@ -7,10 +7,15 @@ use std::collections::HashMap;
 
 mod ui;
 
-struct LoadedFont {
+struct TSGLChar {
     width: usize,
     height: usize,
-    HashMap: HashMap<char, Vec<u8>>,
+    bitmap: Vec<u8>,
+}
+
+struct LoadedFont {
+    positionInfo: Vec<usize>,
+    HashMap: HashMap<char, Vec<TSGLChar>>,
 }
 
 fn gen_ascii(start: char, end: char) -> String {
@@ -25,7 +30,7 @@ fn parse(path: &Path, px: f32, charmaps: &Vec<String>) -> LoadedFont {
     let font = fs::read(path).unwrap();
     let font = font.as_slice();
     let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
-    
+
 
     for charmap in charmaps {
         for (i, c) in charmap.chars().enumerate() {
