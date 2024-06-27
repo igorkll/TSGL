dfb14cfd647fbf98299a5d8f27633800579bdbf7
igorkll <frerther@gmail.com>
2024-06-27 13:56:48 +0300

    Update main.rs

--- tools/tsgl_tool/src/main.rs
+++ tools/tsgl_tool/src/main.rs
@@ -6,7 +6,13 @@ use std::path::*;
 
 mod ui;
 
-fn parse(path: &Path) {
+struct LoadedFont {
+    width: usize,
+    height: usize,
+    bitmap: Vec<u8>,
+}
+
+fn parse(path: &Path, charmaps: &Vec<String>) -> LoadedFont {
     let font = fs::read(path).unwrap();
     let font = font.as_slice();
     let font = fontdue::Font::from_bytes(font, fontdue::FontSettings::default()).unwrap();
@@ -22,13 +28,27 @@ fn parse(path: &Path) {
         }
         println!("");
     }
+
+    for charmap in charmaps {
+        println!("{}", charmap);
+    }
+
+    return LoadedFont {
+        width: metrics.width,
+        height: metrics.height,
+        bitmap: bitmap
+    };
 }
 
 fn main() {
     let result = nfd::open_file_dialog(None, None).unwrap();
 
     match result {
-        Response::Okay(path) => parse(&Path::new(&path)),
+        Response::Okay(path) => {
+            let mut charmaps: Vec<String> = Vec::new();
+            charmaps.push(String::from("Привет"));
+            parse(&Path::new(&path), &charmaps);
+        },
         Response::Cancel => println!("User canceled"),
         _ => ()
     }
