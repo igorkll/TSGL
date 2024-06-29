60ee5ce498b7940e711e5e3d170f3dbb48c22ef3
igorkll <frerther@gmail.com>
2024-06-29 10:50:03 +0300

    gui writing

--- gfxTSGL/TSGL_gui.c
+++ gfxTSGL/TSGL_gui.c
@@ -5,29 +5,40 @@
 #include "TSGL_gui.h"
 
 tsgl_gui* tsgl_gui_createForDisplay(tsgl_display* display) {
-    tsgl_gui* gui = malloc(sizeof(tsgl_display));
-    gui->containers = NULL;
+    tsgl_gui* gui = malloc(sizeof(tsgl_gui));
+    gui->parents = NULL;
     gui->target = display;
     gui->buffered = false;
     return gui;
 }
 
 tsgl_gui* tsgl_gui_createForBuffer(tsgl_framebuffer* framebuffer) {
-    tsgl_gui* gui = malloc(sizeof(tsgl_framebuffer));
-    gui->containers = NULL;
+    tsgl_gui* gui = malloc(sizeof(tsgl_gui));
+    gui->parents = NULL;
     gui->target = framebuffer;
     gui->buffered = true;
     return gui;
 }
 
-tsgl_gui_container* tsgl_gui_addContainer(tsgl_gui* gui, tsgl_pos x, tsgl_pos y, tsgl_pos width, tsgl_pos height, tsgl_rawcolor color) {
-    return NULL;
-}
-
-void tsgl_gui_freeContainer(tsgl_gui_container* container) {
-
-}
-
-void tsgl_gui_free(tsgl_gui* gui) {
-    free(gui);
+void tsgl_gui_free(tsgl_gui_object* object) {
+    if (object->data != NULL) free(object->data);
+    if (object->parents != NULL) {
+        for (size_t i = 0; i < object->parentsCount; i++) {
+            tsgl_gui_freeObject(object->parents[i]);
+        }
+    }
+    if (object->parent != NULL) {
+        for (size_t i = 0; i < object->parent->parentsCount; i++) {
+            if (object->parent->parents[i] == object) {
+                i++;
+                for (; i < object->parent->parentsCount; i++) {
+                    object->parent->parents[i - 1] = object->parent->parents[i];
+                }
+                object->parent->parentsCount--;
+                object->parent->parents = realloc(object->parent->parents, object->parent->parentsCount * sizeof(tsgl_gui_object));
+                break;
+            }
+        }
+    }
+    free(object);
 }
--- gfxTSGL/TSGL_gui.h
+++ gfxTSGL/TSGL_gui.h
@@ -4,23 +4,27 @@
 #include "TSGL_framebuffer.h"
 #include "TSGL_display.h"
 
-typedef struct {
+typedef struct tsgl_gui_object tsgl_gui_object;
+
+struct tsgl_gui_object {
+    size_t parentsCount;
+    tsgl_gui_object** parents;
+    tsgl_gui_object* parent;
+
+    void* target;
+    bool buffered;
+
     tsgl_pos x;
     tsgl_pos y;
     tsgl_pos width;
     tsgl_pos height;
-    tsgl_rawcolor color;
-    tsgl_gui* gui;
-} tsgl_gui_container;
 
-typedef struct {
-    tsgl_gui_container* containers;
-    void* target;
-    bool buffered;
-} tsgl_gui;
+    bool interactive;
+    bool displayable;
 
-tsgl_gui* tsgl_gui_createForDisplay(tsgl_display* display);
-tsgl_gui* tsgl_gui_createForBuffer(tsgl_framebuffer* framebuffer);
-tsgl_gui_container* tsgl_gui_addContainer(tsgl_gui* gui, tsgl_pos x, tsgl_pos y, tsgl_pos width, tsgl_pos height, tsgl_rawcolor color);
-void tsgl_gui_freeContainer(tsgl_gui_container* container);
-void tsgl_gui_free(tsgl_gui* gui);
+    void* data;
+};
+
+tsgl_gui_object* tsgl_gui_createRoot_display(tsgl_gui* gui, tsgl_display* display);
+tsgl_gui_object* tsgl_gui_createRoot_buffer(tsgl_gui* gui, tsgl_framebuffer* framebuffer);
+void tsgl_gui_free(tsgl_gui_object* object);
