8fc664d4ed2583b89d17a64f83cd87175d0ca62f
igorkll <frerther@gmail.com>
2024-07-05 17:43:06 +0300

    Update button.c

--- gfxTSGL/TSGL_gui/button.c
+++ gfxTSGL/TSGL_gui/button.c
@@ -40,7 +40,7 @@ tsgl_gui* tsgl_gui_addButton(tsgl_gui* gui) {
     data->color = TSGL_BLUE;
     data->pressedColor = tsgl_color_mul(data->color, 0.8);
     data->text = text;
-    obj->animationSpeedUpMul = 1;
+    obj->animationSpeedUpMul = 1.5;
     obj->animationSpeedDownMul = 0.5;
     obj->data = data;
     obj->event_callback = _event_callback;
