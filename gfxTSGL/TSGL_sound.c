a30ec229521b32ffbde49ef309bd631b8d649fe0
igorkll <frerther@gmail.com>
2024-07-07 21:58:15 +0300

    backup

--- gfxTSGL/TSGL_sound.c
+++ gfxTSGL/TSGL_sound.c
@@ -1,13 +1,25 @@
 #if __has_include(<driver/dac.h>)
 #include "TSGL_sound.h"
 #include "TSGL_filesystem.h"
-#include <driver/dac.h>
 #include <driver/timer.h>
 #include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-esp_err_t tsgl_sound_load_raw(tsgl_sound* sound, const char* path, size_t sample_rate, size_t bit_rate, size_t channels) {
+static const char* TAG = "TSGL_sound";
+
+static void _playerTask(void* _sound) {
+    tsgl_sound* sound = _sound;
+    dac_output_voltage(DAC_CHAN_0, *(audio->data + wav_pos));
+}
+
+esp_err_t tsgl_sound_load_pcm(tsgl_sound* sound, const char* path, size_t sample_rate, size_t bit_rate, size_t channels) {
     FILE* file = fopen(path, "rb");
     if (file == NULL) return ESP_FAIL;
+    sound->playing = false;
+    sound->pause = false;
+    sound->position = 0;
+    sound->speed = 1.0;
     sound->len = tsgl_filesystem_getFileSize(path);
     sound->sample_rate = sample_rate;
     sound->bit_rate = bit_rate;
@@ -15,6 +27,21 @@ esp_err_t tsgl_sound_load_raw(tsgl_sound* sound, const char* path, size_t sample
     fread(sound->data, sound->bit_rate, sound->len, file);
     fclose(file);
     return ESP_OK;
+}
+
+void tsgl_sound_play(tsgl_sound* sound, dac_channel_t channel) {
+    if (sound->playing) {
+        ESP_LOGE(TAG, "tsgl_sound_play skipped. the track is already playing");
+        return;
+    }
+
+    sound->playing = true;
+    sound->channel = channel;
+
+}
+
+void tsgl_sound_stop(tsgl_sound* sound) {
+    sound->playing = false;
 }
 
 void tsgl_sound_free(tsgl_sound* sound) {
--- gfxTSGL/TSGL_sound.h
+++ gfxTSGL/TSGL_sound.h
@@ -1,15 +1,26 @@
 #pragma once
 #if __has_include(<driver/dac.h>)
 #include "TSGL.h"
+#include <driver/dac.h>
 
 typedef struct {
+    bool playing; //get
+    bool pause; //get
+
+    size_t position; //set / get
+    float speed; //set / get
+
     void* data;
     size_t len;
     size_t sample_rate;
     size_t bit_rate;
     size_t channels;
+
+    dac_channel_t channel;
 } tsgl_sound;
 
 esp_err_t tsgl_sound_load_pcm(tsgl_sound* sound, const char* path, size_t sample_rate, size_t bit_rate, size_t channels); //the bitrate is set not in bits but in bytes
+void tsgl_sound_play(tsgl_sound* sound, dac_channel_t channel);
+void tsgl_sound_stop(tsgl_sound* sound);
 void tsgl_sound_free(tsgl_sound* sound);
 #endif
