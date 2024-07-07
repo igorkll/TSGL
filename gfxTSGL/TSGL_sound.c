3ba224b5b9a058e278f35304e660a8ae7cb4950e
igorkll <frerther@gmail.com>
2024-07-07 22:21:17 +0300

    backup

--- gfxTSGL/TSGL_sound.c
+++ gfxTSGL/TSGL_sound.c
@@ -3,14 +3,25 @@
 #include "TSGL_filesystem.h"
 #include <driver/timer.h>
 #include <stdio.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+#include <esp_attr.h>
 
 static const char* TAG = "TSGL_sound";
 
-static void _playerTask(void* _sound) {
+static void IRAM_ATTR _timer_ISR(void* _sound) {
     tsgl_sound* sound = _sound;
-    dac_output_voltage(DAC_CHAN_0, *(audio->data + wav_pos));
+    timer_group_clr_intr_status_in_isr(sound->timerGroup, sound->timer);
+    if (sound->position >= sound->len) {
+        sound->position = 0;
+        if (sound->loop) {
+            timer_group_enable_alarm_in_isr(sound->timerGroup, sound->timer);
+        } else {
+            sound->playing = false;
+        }
+    } else {
+        timer_group_enable_alarm_in_isr(sound->timerGroup, sound->timer);
+    }
+    dac_output_voltage(DAC_CHANNEL_1, *(((uint8_t*)sound->data) + sound->position));
+    sound->position += sound->bit_rate;
 }
 
 esp_err_t tsgl_sound_load_pcm(tsgl_sound* sound, const char* path, size_t sample_rate, size_t bit_rate, size_t channels) {
@@ -39,6 +50,24 @@ void tsgl_sound_play(tsgl_sound* sound, dac_channel_t channel) {
     sound->playing = true;
     sound->channel = channel;
 
+    timer_config_t config = {
+		.divider = 8,
+		.counter_dir = TIMER_COUNT_UP,
+		.counter_en = TIMER_PAUSE,
+		.alarm_en = TIMER_ALARM_EN,
+		.intr_type = TIMER_INTR_LEVEL,
+		.auto_reload = 1
+	};
+
+    sound->timerGroup = TIMER_GROUP_0;
+    sound->timer = TIMER_0;
+
+	ESP_ERROR_CHECK(timer_init(sound->timerGroup, sound->timer, &config));
+	ESP_ERROR_CHECK(timer_set_counter_value(sound->timerGroup, sound->timer, 0x00000000ULL));
+	ESP_ERROR_CHECK(timer_set_alarm_value(sound->timerGroup, sound->timer, TIMER_BASE_CLK / config.divider / sound_wav_info.sampleRate));
+	ESP_ERROR_CHECK(timer_enable_intr(sound->timerGroup, sound->timer));
+	timer_isr_register(sound->timerGroup, sound->timer, _timer_ISR, &sound, ESP_INTR_FLAG_IRAM, NULL);
+	timer_start(sound->timerGroup, sound->timer);
 }
 
 void tsgl_sound_stop(tsgl_sound* sound) {
@@ -47,6 +76,7 @@ void tsgl_sound_stop(tsgl_sound* sound) {
         return;
     }
 
+    timer_pause(sound->timerGroup, sound->timer);
     ESP_ERROR_CHECK_WITHOUT_ABORT(dac_output_disable(sound->channel));
     sound->playing = false;
 }
--- gfxTSGL/TSGL_sound.h
+++ gfxTSGL/TSGL_sound.h
@@ -6,7 +6,8 @@
 typedef struct {
     bool playing; //get
     bool pause; //get
-
+    
+    bool loop; //set / get
     size_t position; //set / get
     float speed; //set / get
 
@@ -17,9 +18,13 @@ typedef struct {
     size_t channels;
 
     dac_channel_t channel;
+    timer_idx_t timer;
+    timer_group_t timerGroup;
 } tsgl_sound;
 
-esp_err_t tsgl_sound_load_pcm(tsgl_sound* sound, const char* path, size_t sample_rate, size_t bit_rate, size_t channels); //the bitrate is set not in bits but in bytes
+//the bitrate is set not in bits but in bytes
+//however, due to the features of the DAC in esp32, it does not make sense to use more than 8 bit (this will not increase the sound quality)
+esp_err_t tsgl_sound_load_pcm(tsgl_sound* sound, const char* path, size_t sample_rate, size_t bit_rate, size_t channels);
 void tsgl_sound_play(tsgl_sound* sound, dac_channel_t channel);
 void tsgl_sound_stop(tsgl_sound* sound);
 void tsgl_sound_free(tsgl_sound* sound);
