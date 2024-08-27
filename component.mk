29ee8a5d5556dca8b3ab6cb0e12fd1e3239063bf
igorkll <frerther@gmail.com>
2024-08-27 23:59:44 +0300

    fuck the esp-idf component system

--- component.mk
+++
@@ -1,7 +0,0 @@
-COMPONENT_SRCDIRS := . \
-                  TSGL \
-                  TSGL/TSGL_drivers \
-                  TSGL/TSGL_fonts \
-                  TSGL/TSGL_gui
-
-COMPONENT_ADD_INCLUDEDIRS := $(COMPONENT_SRCDIRS) .
